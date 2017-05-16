9fff169345ef44f377053a40fa9c47a2637d4422
sibolwolf <hejinlai@foxmail.com>
2017-05-16 16:54:24 +0800

    add SysLockUpdate interface
    Change-Id: I1936e36df53d9c77c3643650d2ae90ae9a949b33

--- sysctrller.go
+++ sysctrller.go
@@ -9,36 +9,54 @@ import (
     SysSW       "smartconn.cc/sibolwolf/syssleepwake"
 )
 
-func SysSWTest() {
-    // Nothing to do
-    SysSW.UpdateLockStatus("audiolock", 0)
-    time.Sleep(time.Second * 5)
-    SysSW.UpdateLockStatus("storysynclock", 1)
-    time.Sleep(time.Second * 5)
-    SysSW.UpdateLockStatus("storysynclock", 0)
-    time.Sleep(time.Second * 1)
-    SysSW.UpdateLockStatus("storysynclock", 1)
-    time.Sleep(time.Second * 1)
-    SysSW.UpdateLockStatus("storydecompresslock", 1)
-    time.Sleep(time.Second * 5)
-    SysSW.UpdateLockStatus("storysynclock", 0)
-    time.Sleep(time.Second * 5)
-    SysSW.UpdateLockStatus("storydecompresslock", 0)
-    SysSW.UpdateLockStatus("storydecompresslock", 1)
-    SysSW.UpdateLockStatus("storydecompresslock", 0)
-    SysSW.UpdateLockStatus("storydecompresslock", 1)
-    SysSW.UpdateLockStatus("storydecompresslock", 0)
-}
-
-func main(){
+func Init() {
     // Init syssleepwake package
-    log.Println("Hello, SysSleepWake")
+    log.Println("Hello, Init SysSleepWake ...")
     SysSW.Init()
-    go SysSWTest()
 
     // Init volumectroller package
-    log.Println("Hello, VolumeController")
+    log.Println("Hello, Init VolumeController ...")
     VolCtrller.Init()
+}
+
+func SysLockUpdate(lockname string, value int) {
+    /*
+    cameralock
+    audiolock
+    storydownloadlock
+    storydecompresslock
+    storysynclock
+    */
+    SysSW.UpdateLockStatus(lockname, value)
+}
+
+func SysSWTest() {
+    // Nothing to do
+    SysLockUpdate("audiolock", 0)
+    time.Sleep(time.Second * 5)
+    SysLockUpdate("storysynclock", 1)
+    time.Sleep(time.Second * 5)
+    SysLockUpdate("storysynclock", 0)
+    time.Sleep(time.Second * 1)
+    SysLockUpdate("storysynclock", 1)
+    time.Sleep(time.Second * 1)
+    SysLockUpdate("storydecompresslock", 1)
+    time.Sleep(time.Second * 5)
+    SysLockUpdate("storysynclock", 0)
+    time.Sleep(time.Second * 5)
+    SysLockUpdate("storydecompresslock", 0)
+    SysLockUpdate("storydecompresslock", 1)
+    SysLockUpdate("storydecompresslock", 0)
+    SysLockUpdate("storydecompresslock", 1)
+    SysLockUpdate("storydecompresslock", 0)
+}
+
+func main() {
+    log.Println("Hello, this is sysctrller module ...")
+    Init()
+
+    // Test module
+    go SysSWTest()
 
     signalChanel := make(chan os.Signal, 1)
     signal.Notify(signalChanel, os.Interrupt)
